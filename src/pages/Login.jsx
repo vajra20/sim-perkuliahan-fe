d9ea48faa3e5f1dacaf0f0c9af6f6b7b521d1039
Ryan Pratama Hidayat <ryan.pratama.h11@gmail.com>
2024-01-03 08:00:30 +0700

    update(login): update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,7 +6,6 @@ import Swal from "sweetalert2";
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
-import Swal from "sweetalert2";
 
 const Login = () => {
 	const navigate = useNavigate();
