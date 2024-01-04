1edea3859df7ebc43b950ad4a46ad1a885f4a3e3
Ryan Pratama Hidayat <ryan.pratama.h11@gmail.com>
2024-01-04 12:03:02 +0700

    update(admin): fetching api for dosen, mahasiswa, and dashboard admin

--- src/pages/Admin/Dosen.jsx
+++ src/pages/Admin/Dosen.jsx
@@ -15,78 +15,20 @@ import editIcon from "@iconify/icons-tabler/edit";
 import trashIcon from "@iconify/icons-ion/trash";
 
 const Dosen = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isModalEditOpen, setIsModalEditOpen] = useState(false);
-  const [isModalDeleteOpen, setIsModalDeleteOpen] = useState(false);
+	// Dosen
+	const [dosenData, setDosenData] = useState([]);
 
-const columns = [
-	{
-		title: "No",
-		dataIndex: "no",
-		key: "no",
-	},
-	{
-		title: "Nama",
-		dataIndex: "nama",
-		key: "nama",
-	},
-	{
-		title: "Mata Pelajaran",
-		dataIndex: "mapel",
-		key: "mapel",
-	},
-	{
-		title: "NIP",
-		dataIndex: "nip",
-		key: "nip",
-	},
-	{
-		title: "Action",
-		key: "action",
-		render: (_, record) => (
-			<Space size="middle">
-				<button className="bg-[#FFC006] p-1 rounded ">
-					<Icon icon={editIcon} className=" w-5 h-5"></Icon>
-				</button>
-				<button className="bg-[#DA3442] p-1 rounded ">
-					<Icon
-						icon={trashIcon}
-						className="text-white w-5 h-5"
-					></Icon>
-				</button>
-			</Space>
-		),
-	},
-];
+	// Fetching Dosen Data ( find All )
+	useEffect(() => {
+		const fetchDosenData = async () => {
+			const DosenData = await getDosenData();
+			setDosenData(DosenData);
+		};
 
-const data = [
-	{
-		no: "1",
-		nama: "Vito Aleandra S. Kom",
-		mapel: "Teknik Komputer",
-		nip: 12108545,
-	},
-	{
-		no: "2",
-		nama: "Maulana Yusuf S.Pd",
-		mapel: "Matematika",
-		nip: 12108878,
-	},
-	{
-		no: "3",
-		nama: "Benediktus Vajra Sagara S. Kom",
-		mapel: "Sejarah",
-		nip: 12108356,
-	},
-];
+		fetchDosenData();
+	}, []);
 
-const paginationConfig = {
-	pageSize: 10,
-	showTotal: (total, range) =>
-		`Showing ${range[0]} - ${range[1]} of ${total} list`,
-};
-
-const Dosen = () => {
+	// Modal
 	const [isModalOpen, setIsModalOpen] = useState(false);
 
 	const showModal = () => {
@@ -101,31 +43,92 @@ const Dosen = () => {
 		setIsModalOpen(false);
 	};
 
-  return (
-    <div className="w-full h-full">
-      <NavbarDashboard />
-      <div className="md:px-7 lg:py-6 android:p-3">
-        <div className="bg-white border rounded-xl shadow ">
-          <div className=" w-full border-b-2 border-event-color md:px-10 android:px-5 py-3 flex justify-between items-center">
-            <span className="text-color-page text-2xl font-medium">
-              Akun Dosen
-            </span>
+	// Table Settings
+	const columns = [
+		{
+			title: "No",
+			dataIndex: "no",
+			key: "no",
+		},
+		{
+			title: "Nama",
+			dataIndex: "dosen_name",
+			key: "dosen_name",
+		},
+		{
+			title: "Mata Pelajaran",
+			dataIndex: "matkul",
+			key: "matkul",
+		},
+		{
+			title: "NIP",
+			dataIndex: "nip",
+			key: "nip",
+		},
+		{
+			title: "Action",
+			key: "action",
+			render: (_, record) => (
+				<Space size="middle">
+					<button className="bg-[#FFC006] p-1 rounded ">
+						<Icon icon={editIcon} className=" w-5 h-5"></Icon>
+					</button>
 
-            <button onClick={showModal}>
-              <div className="flex flex-row items-center">
-                <div className="md:px-6 android:px-4 shadow rounded-l border border-event-color android:h-7 md:h-9 flex items-center">
-                  <span className="md:text-lg android:text-sm text-color-page font-medium">
-                    Add
-                  </span>
-                </div>
-                <div className="bg-color-page text-white flex items-center rounded-r p-2 shadow">
-                  <Icon
-                    icon={todoAdd}
-                    className="md:w-5 md:h-5 android:w-3 android:h-3"
-                  ></Icon>
-                </div>
-              </div>
-            </button>
+					<button className="bg-[#DA3442] p-1 rounded ">
+						<Icon
+							icon={trashIcon}
+							className="text-white w-5 h-5"
+						></Icon>
+					</button>
+				</Space>
+			),
+		},
+	];
+
+	const DosenList = dosenData.map((item, index) => {
+		let data;
+
+		data = {
+			no: `${index + 1}.`,
+			dosen_name: item.dosenName,
+			nip: item.nip,
+			matkul: item.matkul.namaMatkul,
+		};
+
+		return data;
+	});
+
+	const paginationConfig = {
+		pageSize: 10,
+		showTotal: (total, range) =>
+			`Showing ${range[0]} - ${range[1]} of ${total} list`,
+	};
+
+	return (
+		<div className="w-full h-full">
+			<NavbarDashboard />
+			<div className="md:px-7 lg:py-6 android:p-3">
+				<div className="bg-white border rounded-xl shadow ">
+					<div className=" w-full border-b-2 border-event-color md:px-10 android:px-5 py-3 flex justify-between items-center">
+						<span className="text-color-page text-2xl font-medium">
+							Akun Dosen
+						</span>
+
+						<button onClick={showModal}>
+							<div className="flex flex-row items-center">
+								<div className="md:px-6 android:px-4 shadow rounded-l border border-event-color android:h-7 md:h-9 flex items-center">
+									<span className="md:text-lg android:text-sm text-color-page font-medium">
+										Add
+									</span>
+								</div>
+								<div className="bg-color-page text-white flex items-center rounded-r p-2 shadow">
+									<Icon
+										icon={todoAdd}
+										className="md:w-5 md:h-5 android:w-3 android:h-3"
+									></Icon>
+								</div>
+							</div>
+						</button>
 
 						<Modal
 							title="Buat Akun"
@@ -160,6 +163,7 @@ const Dosen = () => {
 										/>
 									</div>
 								</div>
+
 								<div className="grid grid-cols-3 items-center">
 									<span className="text-black font-normal sm:text-lg android:text-base">
 										NIP
@@ -175,6 +179,7 @@ const Dosen = () => {
 										/>
 									</div>
 								</div>
+
 								<div className="grid grid-cols-3 items-center">
 									<span className="text-black font-normal sm:text-lg android:text-base">
 										Mata pelajaran
@@ -198,7 +203,7 @@ const Dosen = () => {
 						<Table
 							className="w-full"
 							columns={columns}
-							dataSource={data}
+							dataSource={DosenList}
 							pagination={paginationConfig}
 						/>
 					</div>
